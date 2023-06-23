bceb45221dddc1387b8141ee52d4b8da9b523bc9
Enrique Santos <santosenrique869@gmail.com>
2023-06-23 19:00:59 -0300

    chore: configure domain images

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "cdn.jsdelivr.net/gh/devicons/devicon/icons",
+      "img.icons8.com",
+      "github.com",
+      "media.graphassets.com",
+    ],
+  },
+};
+
+module.exports = nextConfig;
