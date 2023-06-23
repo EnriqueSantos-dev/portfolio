f2b3d8d46b56ce4c6c0b12487df5173e92eb2927
Enrique Santos <santosenrique869@gmail.com>
2023-06-23 19:42:33 -0300

    :wrench:chore: add skill-array constant

---
+++ src/constants/skills-array.ts
@@ -0,0 +1,66 @@
+import PrismaOrmSvg from "@/assets/prisma.svg";
+
+export const skills = [
+	{
+		name: "ReactJS",
+		icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+	},
+	{
+		name: "NextJS",
+		icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
+		isInvertInDark: true,
+	},
+	{
+		name: "TailwindCSS",
+		icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+	},
+	{
+		name: "Styled Components",
+		icon: "https://img.icons8.com/color/48/styled-components.png",
+	},
+	{
+		name: "NodeJS",
+		icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+	},
+	{
+		name: "ExpressJS",
+		icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+		isInvertInDark: true,
+	},
+	{
+		name: "PostgreSQL",
+		icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
+	},
+	{
+		name: "NestJS",
+		icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nestjs/nestjs-plain.svg",
+	},
+	{
+		name: "TypeScript",
+		icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+	},
+	{
+		name: "JavaScript",
+		icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+	},
+	{
+		name: "Jest",
+		icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg",
+	},
+	{
+		name: "Docker",
+		icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg",
+	},
+	{
+		name: "Git",
+		icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+	},
+	{
+		name: "Sass",
+		icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg",
+	},
+	{
+		name: "Prisma ORM",
+		icon: PrismaOrmSvg,
+	},
+];
