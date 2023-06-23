c50e93d4d0112a2e5d00d8d8c4cfb7da2098cdc3
Enrique Santos <santosenrique869@gmail.com>
2023-06-23 19:01:32 -0300

    feat: create useTheme hook

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,62 @@
+import { useState, useCallback, useEffect } from "react";
+import { THEME_KEY } from "@/constants/local-storage-keys";
+
+type Theme = "light" | "dark";
+
+export default function useTheme() {
+	const [theme, setTheme] = useState<Theme | null>(null);
+
+	const toggleTheme = useCallback(() => {
+		const documentElement = window.document.documentElement;
+		const currentThemeInDocumentIsDark =
+			documentElement.classList.contains("dark");
+		const currentThemeInDocumentIsLight =
+			documentElement.classList.contains("light");
+
+		if (!currentThemeInDocumentIsDark && !currentThemeInDocumentIsLight) {
+			documentElement.classList.add("dark");
+			setTheme("dark");
+		}
+
+		if (currentThemeInDocumentIsDark) {
+			documentElement.classList.remove("dark");
+			documentElement.classList.add("light");
+		}
+
+		if (currentThemeInDocumentIsLight) {
+			documentElement.classList.remove("light");
+			documentElement.classList.add("dark");
+		}
+
+		setTheme((prev) => {
+			const currentThemToSet = prev === "light" || !prev ? "dark" : "light";
+			localStorage.setItem(THEME_KEY, currentThemToSet);
+			return currentThemToSet;
+		});
+	}, []);
+
+	useEffect(() => {
+		const documentElement = window.document.documentElement;
+
+		// change theme if user has already set a theme, or if user has a system preference
+		const themeFromLocalStorage = localStorage.getItem(THEME_KEY) as Theme;
+		const systemPreferenceIsDark = window.matchMedia(
+			"(prefers-color-scheme: dark)"
+		).matches;
+
+		if (themeFromLocalStorage) {
+			setTheme(themeFromLocalStorage);
+			documentElement.classList.add(themeFromLocalStorage);
+			return;
+		}
+
+		if (systemPreferenceIsDark) {
+			setTheme("dark");
+			localStorage.setItem(THEME_KEY, "dark");
+			documentElement.classList.add("dark");
+			return;
+		}
+	}, []);
+
+	return [theme, toggleTheme] as const;
+}
