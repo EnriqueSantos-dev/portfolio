314b180e7c761297b71be78e981c1beab9bf114d
Enrique Santos <santosenrique869@gmail.com>
2023-06-24 00:31:39 -0300

    :recycle:refactor(style): improve card width for mobile device

--- src/components/card-skill-tech.tsx
+++ src/components/card-skill-tech.tsx
@@ -31,17 +31,19 @@ export default function CardSkillTech({
 					<motion.button
 						{...motionConfig}
 						aria-label={`tech card for ${tooltip}`}
-						className="flex h-28 w-28 items-center justify-center rounded-full border border-neutral-300 bg-neutral-100 ring-amber-500 hover:scale-105 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-neutral-100 dark:border-neutral-800 dark:bg-neutral-950/60 dark:shadow-[inset_0_0_0_3px_rgb(20,20,20)] dark:focus-visible:ring-offset-neutral-900"
+						className="flex h-24 w-24 items-center justify-center rounded-full border border-neutral-300 bg-neutral-100 ring-amber-500 hover:scale-105 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-neutral-100 dark:border-neutral-800 dark:bg-neutral-950/60 dark:shadow-[inset_0_0_0_3px_rgb(20,20,20)] dark:focus-visible:ring-offset-neutral-900 md:h-28 md:w-28"
 					>
-						<Image
-							src={iconUrl}
-							alt="tech icon"
-							width={50}
-							height={50}
-							className={cn({
-								"dark:invert": isInvertInDark,
-							})}
-						/>
+						<span className="relative h-10 w-10 md:h-12 md:w-12">
+							<Image
+								src={iconUrl}
+								alt="tech icon"
+								fill
+								sizes="50px"
+								className={cn({
+									"dark:invert": isInvertInDark,
+								})}
+							/>
+						</span>
 					</motion.button>
 				</TooltipTrigger>
 
