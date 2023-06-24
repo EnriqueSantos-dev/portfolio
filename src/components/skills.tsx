9f5921dd398cb7e0c3091f77f3b62cffef7f411d
Enrique Santos <santosenrique869@gmail.com>
2023-06-24 13:58:48 -0300

    :sparkles:feat: add subheading

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { skills } from "@/constants/skills-array";
-import { AnimatePresence, useInView } from "framer-motion";
+import { AnimatePresence, inView, useInView } from "framer-motion";
 import { useRef } from "react";
 import CardSkillTech from "./card-skill-tech";
 
@@ -9,6 +9,7 @@ type SkillsProps = {
 	sectionId: string;
 	dictionary: {
 		heading: string;
+		subheading: string;
 	};
 };
 
@@ -22,31 +23,34 @@ export function Skills({ dictionary, sectionId }: SkillsProps) {
 				<h2 className="gradient-highlight mb-2 block text-3xl font-bold md:text-4xl/10">
 					{dictionary.heading}
 				</h2>
+
+				<p className="mx-auto text-neutral-600 dark:text-neutral-300 lg:w-2/4">
+					{dictionary.subheading}
+				</p>
 			</div>
 
 			<div
 				className="mx-auto mt-10 flex flex-wrap items-center justify-around gap-4 lg:justify-center xl:max-w-[80%]"
 				ref={containerRef}
 			>
-				{skills.map(
-					(skill, i) =>
-						isInView && (
-							<AnimatePresence key={skill.name}>
-								<CardSkillTech
-									key={skill.name}
-									iconUrl={skill.icon}
-									tooltip={skill.name}
-									isInvertInDark={!!skill.isInvertInDark}
-									motionConfig={{
-										initial: { opacity: 0, scale: 0 },
-										animate: { opacity: 1, scale: 1 },
-										exit: { opacity: 0, scale: 0 },
-										transition: { duration: 0.2, delay: i * 0.1 },
-									}}
-								/>
-							</AnimatePresence>
-						)
-				)}
+				{skills.map((skill, i) => (
+					<AnimatePresence key={skill.name}>
+						{isInView && (
+							<CardSkillTech
+								key={skill.name}
+								iconUrl={skill.icon}
+								tooltip={skill.name}
+								isInvertInDark={!!skill.isInvertInDark}
+								motionConfig={{
+									initial: { opacity: 0, scale: 0 },
+									animate: { opacity: 1, scale: 1 },
+									exit: { opacity: 0, scale: 0 },
+									transition: { duration: 0.2, delay: i * 0.1 },
+								}}
+							/>
+						)}
+					</AnimatePresence>
+				))}
 			</div>
 		</section>
 	);
