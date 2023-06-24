ac90cff26793520d9f2257dd52d0014c619fe837
Enrique Santos <santosenrique869@gmail.com>
2023-06-24 17:16:04 -0300

    :bug:fix: incorrect linkedin link

--- src/constants/social.ts
+++ src/constants/social.ts
@@ -1,4 +1,4 @@
 export const socialLinks = {
 	github: "https://github.com/enriquesantos-dev",
-	linkedin: "https://www.linkedin.com/in/enrique-santos-1b1b3b1b2/",
+	linkedin: "https://www.linkedin.com/in/enrique-santos-923167166",
 } as const;
