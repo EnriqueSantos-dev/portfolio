39a21cd9800e4b80064b0bfc7fee184c58201b86
Enrique Santos <santosenrique869@gmail.com>
2023-06-26 18:33:04 -0300

    :recycle:refactor: change limite items and query

--- src/services/get-projects.ts
+++ src/services/get-projects.ts
@@ -1,55 +1,58 @@
 import { GetProjectsResponse } from "@/types";
 
-export const LIMITE_PROJECTS = 10;
+export const LIMITE_PROJECTS = 6;
 
 const query = `
-  query ($locale: [Locale!]!, $skip: Int!) {
-    projects(locales: $locale, skip: $skip, orderBy: relevance_ASC) {
-      id
-      name
-      description
-      deployLink
-      backgroundCover {
-        url
-        width
-        height
-      }
-      tags
-      createdAt
-      updatedAt
-      relevance
-      repositoryUrl
-    }
-    projectsConnection {
-      pageInfo {
-        pageSize
-        hasNextPage
-        hasPreviousPage
-      }
-    }
+  query getAllProjects ($locale: [Locale!]!, $skip: Int!, $first: Int!) {
+    projectsConnection(locales: $locale, skip: $skip, first: $first) {
+			edges {
+				node {
+					id
+					description
+					name
+					createdAt
+					updatedAt
+					deployLink
+					tags
+					repositoryUrl
+					relevance
+					backgroundCover {
+						url
+						width
+						height
+					}
+				}  
+			}
+			pageInfo {
+				pageSize
+				hasNextPage
+				hasPreviousPage
+			}
+		}
   }
 `;
 
 export async function getAllProjects(
 	locale: string = "pt_BR",
-	skip: number = 0
+	skip: number = 0,
+	first: number = LIMITE_PROJECTS
 ): Promise<GetProjectsResponse> {
 	const headers = {
 		"Content-Type": "application/json",
-		Authorization: `Bearer ${process.env.NEXT_PUBLIC_HYGRAPH_PERMANENT_AUTH_TOKEN}`,
+		Authorization: `Bearer ${process.env.HYGRAPH_PERMANENT_AUTH_TOKEN}`,
 	};
 
 	const body = {
 		query,
-		variables: { locale: [locale], skip },
+		variables: { locale: [locale], skip, first },
 	};
 
-	const res = await fetch(process.env.NEXT_PUBLIC_HYGRAPH_URL as string, {
+	const res = await fetch(process.env.HYGRAPH_URL as string, {
 		method: "POST",
 		headers,
 		body: JSON.stringify(body),
 		next: {
-			revalidate: 60 * 60 * 24, // 24 hours
+			revalidate: 60 * 60 * 24, // 1 day
 		},
 	});
 
