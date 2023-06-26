9ae20bfdc83e991077d636ae98386bd10d6a6894
Enrique Santos <santosenrique869@gmail.com>
2023-06-26 18:33:42 -0300

    :recycle:refactor: add variables as part of query key

--- src/hooks/useGetProjects.ts
+++ src/hooks/useGetProjects.ts
@@ -7,18 +7,23 @@ export function useGetProjects({
 	locale = "pt_BR",
 	skip = 0,
 	initialData,
+	first = 6,
 }: {
 	locale?: string;
 	skip?: number;
+	first?: number;
 	initialData?: GetProjectsResponse;
 }) {
 	return useQuery({
-		queryKey: ["projects"],
-		queryFn: () =>
-			fetch(`/api/projects?locale=${locale}&skip=${skip}`).then((res) =>
-				res.json()
-			),
-		staleTime: 1000 * 60 * 60 * 24,
-		initialData: initialData ?? undefined,
+		queryKey: ["projects", skip, first] as const,
+		queryFn: async () => {
+			const res = await fetch(
+				`/api/projects?locale=${locale}&skip=${skip}&first=${first}`
+			);
+			const data = await res.json();
+			return data as GetProjectsResponse;
+		},
+		staleTime: 60 * 60 * 24, // 1 day
+		initialData: initialData,
 	});
 }
