5fe10e590a48824728c1aface34bb6a855ce97e2
Enrique Santos <santosenrique869@gmail.com>
2023-06-26 23:07:44 -0300

    :wrench:chore: export Dictionary type to use in other parts in app

--- src/utils/mappers-i18n.ts
+++ src/utils/mappers-i18n.ts
@@ -1,6 +1,6 @@
 import { getDictionary } from "@/i18n/get-dictionary";
 
-type Dictionary = Awaited<ReturnType<typeof getDictionary>>;
+export type Dictionary = Awaited<ReturnType<typeof getDictionary>>;
 
 export function mapperDictValuesFromKey<TKey extends keyof Dictionary>(
 	dictionary: Dictionary,
