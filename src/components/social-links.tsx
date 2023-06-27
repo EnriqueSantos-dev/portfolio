8bd6da1a9d15895d09af28c2c7a2a0ca0f44b444
Enrique Santos <santosenrique869@gmail.com>
2023-06-26 23:21:14 -0300

    :sparkles:feat: create social-links component

---
+++ src/components/social-links.tsx
@@ -0,0 +1,27 @@
+import { socialLinks } from "@/constants/social";
+import { Github, Linkedin } from "lucide-react";
+import Link from "next/link";
+
+export function SocialLinks() {
+	return (
+		<div className="flex items-center gap-2">
+			<Link
+				href={socialLinks.github}
+				target="_blank"
+				aria-label="Navigate to github profile"
+				className="inline-flex h-9 cursor-pointer items-center gap-2 rounded-md border border-neutral-200 p-2 font-medium text-neutral-900 ring-amber-500 ring-offset-neutral-50 transition-colors hover:bg-neutral-100 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-2 dark:border-neutral-800 dark:bg-neutral-950/50 dark:text-white dark:hover:border-zinc-700 dark:hover:bg-neutral-800 dark:focus-visible:ring-offset-neutral-900"
+			>
+				<Github size={18} />
+			</Link>
+
+			<Link
+				href={socialLinks.linkedin}
+				target="_blank"
+				aria-label="Navigate to linkedin profile"
+				className="inline-flex h-9 cursor-pointer items-center gap-2 rounded-md border border-neutral-200 p-2 font-medium text-neutral-900 ring-amber-500 ring-offset-neutral-50 transition-colors hover:bg-neutral-100 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-2 dark:border-neutral-800 dark:bg-neutral-950/50 dark:text-white dark:hover:border-zinc-700 dark:hover:bg-neutral-800 dark:focus-visible:ring-offset-neutral-900"
+			>
+				<Linkedin size={18} />
+			</Link>
+		</div>
+	);
+}
