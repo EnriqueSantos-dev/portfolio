68ca5b237f86fab6f35f86a3258eed98d2260680
Enrique Santos <santosenrique869@gmail.com>
2023-06-30 13:34:17 -0300

    :recycle:refactor(style): change bg color in light mode

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,8 +3,8 @@ import { SocialLinks } from "./social-links";
 
 export function Footer() {
 	return (
-		<footer className="relative h-[72px] w-full border-t border-neutral-300 bg-neutral-100 py-2 dark:border-neutral-800 dark:bg-neutral-950">
-			<div className="container flex h-full items-center justify-between text-xs text-neutral-600 dark:text-neutral-600 lg:text-sm/3">
+		<footer className="relative h-[72px] w-full border-t border-neutral-200 bg-neutral-50 py-2 dark:border-neutral-800 dark:bg-neutral-950">
+			<div className="container flex h-full items-center justify-between text-xs text-neutral-500 dark:text-neutral-500 lg:text-sm/3">
 				<p className="inline-flex items-center gap-1">
 					Copyright <Copyright size={16} /> 2023 Enrique Santos
 				</p>
