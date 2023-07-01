4f84e34c115033bd06e009a0a6f02d35c9faf1a1
Enrique Santos <santosenrique869@gmail.com>
2023-07-01 11:04:37 -0300

    :recycle:refactor(style): decrease z-index from 10 to 5 to avoid conflict with others elements

--- src/components/scroll-state-page-indicator.tsx
+++ src/components/scroll-state-page-indicator.tsx
@@ -14,7 +14,7 @@ export function ScrollStatePageIndicator() {
 			role="progressbar"
 			aria-label="Page scroll progress indicator"
 			className={cn(
-				"fixed left-0 top-[72px] z-10 h-1 w-full origin-left rounded-lg bg-gradient-to-r to-amber-500 from-red-500",
+				"fixed left-0 top-[72px] z-[5] h-1 w-full origin-left rounded-lg bg-gradient-to-r to-amber-500 from-red-500",
 				{
 					visible: !isMenuOpen,
 					invisible: isMenuOpen,
