038c4c5c88e44bef7b506838e7b88cd084cc0f8a
Enrique Santos <santosenrique869@gmail.com>
2024-04-01 09:34:57 -0300

    :recycle:refactor: add dynamic year in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,7 +7,8 @@ export function Footer() {
 		<footer className="relative h-[72px] w-full border-t border-neutral-200 bg-neutral-50 py-2 dark:border-neutral-800 dark:bg-neutral-950">
 			<div className="container flex h-full items-center justify-between text-xs text-neutral-500 dark:text-neutral-500 lg:text-sm/3">
 				<p className="inline-flex items-center gap-1">
-					Copyright <Copyright size={16} /> 2023 Enrique Santos
+					Copyright <Copyright size={16} /> {new Date().getFullYear()} Enrique
+					Santos
 				</p>
 				<SocialLinks />
 			</div>
